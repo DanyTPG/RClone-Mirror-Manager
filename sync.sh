9d42242c58a537916887009c0de8563d448c3eac
DanyTPG <50212295+DanyTPG@users.noreply.github.com>
2021-11-11 21:54:37 +0330

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
 
-drives=(
-    "test"
-    "one"
-    "two"
-    "three"
-    "four"
-    "five"
-)
+touch p p1440
+rclone lsf D1:Archive/Unsorted/ | grep 1440p.mp4 >> p1440
+for i in $(cat p1440 | uniq); do rclone copy D1:Archive/Unsorted/"$i" D6:Folder/PH/2K --drive-server-side-across-configs; done
+for i in $(cat p1440 | uniq); do rclone delete D1:Archive/Unsorted/"$i" ; done
 
-mirrors=(
-    "1"
-    "2"
-    "3"
-    "4"
-    "5"
-)
+rclone lsf D1:Archive/Unsorted/ | grep XXX >> p
+rclone lsf D1:Archive/Unsorted/ | grep PRT >> p
+for i in $(cat p | uniq); do rclone copy D1:Archive/Unsorted/"$i" D6:Folder/PH/premium --drive-server-side-across-configs; done
+for i in $(cat p | uniq); do rclone delete D1:Archive/Unsorted/"$i" ; done
+rm p p1440
 
-to="${mirrors[1]}"
-for from in "${drives[@]}"; do
-    echo "$from"
-    echo "$to"
-    rclone --config rclone.conf sync --drive-server-side-across-configs "$from": "$to":sync/"$from"
-done
 
-from="${mirrors[1]}"
-for to in "${mirrors[@]:1}"; do
-    echo "$from"
-    echo "$to"
-    rclone --config rclone.conf sync --drive-server-side-across-configs "$from":sync/ "$to":sync/
-done
+rclone sync  D1:Archive D2:Archive --drive-server-side-across-configs --checksum
+rclone sync  D1:Archive D3:Archive --drive-server-side-across-configs --checksum
+rclone sync  D1:Archive D4:Archive --drive-server-side-across-configs --checksum
+rclone sync  D1:Archive D5:Archive --drive-server-side-across-configs --checksum
+#rclone sync  D1:Archive D6:Archive --drive-server-side-across-configs
+rclone sync  D6:Folder D2:Folder --drive-server-side-across-configs --checksum
+rclone sync  D6:Folder D3:Folder --drive-server-side-across-configs --checksum
+rclone sync  D6:Folder D4:Folder --drive-server-side-across-configs --checksum
+rclone sync  D6:Folder D5:Folder --drive-server-side-across-configs --checksum
+
+date
