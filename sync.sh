d7753f4c0c59cd20e5d4cdea2c3922f6ba758999
DanyTPG <50212295+DanyTPG@users.noreply.github.com>
2021-11-23 17:17:47 +0330

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -14,18 +14,18 @@ cat list | grep  Dexter.*1080p.*\.mkv >> Dexter1080
 
 IFS=$'\n'
 
-#for i in $(cat Doom720 | uniq) ; do rclone move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/Doom Patrol/Doom Patrol Season 3 (720p x265 10bit WEBRip PSA)/" --drive-server-side-across-configs ; done
-#for i in $(cat Doom1080 | uniq) ; do rclone move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/Doom Patrol/Doom Patrol Season 3 (1080p x265 10bit WEBRip PSA)/" --drive-server-side-across-configs ; done
-for i in $(cat Legacies720 | uniq) ; do rclone move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/Legacies/Legacies Season 4 (720p x265 10bit WEBRip PSA)/"  --drive-server-side-across-configs ; done
-for i in $(cat Legacies1080 | uniq) ; do rclone move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/Legacies/Legacies Season 4 (1080p x265 10bit WEBRip PSA)/"  --drive-server-side-across-configs ; done
-for i in $(cat Wheel720 | uniq) ; do rclone move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/The Wheel of Time/Season 1 [720p x265 10bit WEBRip PSA]/" --drive-server-side-across-configs ; done
-for i in $(cat Wheel1080 | uniq) ; do rclone move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/The Wheel of Time/Season 1 [1080p x265 10bit WEBRip PSA]/" --drive-server-side-across-configs ; done
-for i in $(cat Mushoku | uniq) ; do rclone move -P D1:Archive/Unsorted/"$i" D1:"Archive/Anime/Mushoku Tensei/" --drive-server-side-across-configs ; done
-for i in $(cat Kimetsu | uniq) ; do rclone move -P D1:Archive/Unsorted/"$i" D1:"Archive/Anime/Demon Slayer (Kimetsu no Yaiba)/" --drive-server-side-across-configs ; done
-for i in $(cat Dexter720 | uniq) ; do rclone move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/Dexter/Dexter Season 9 [720p x265 10bit WEBRip PSA]/"  --drive-server-side-across-configs ; done
-for i in $(cat Dexter1080 | uniq) ; do rclone move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/Dexter/Dexter Season 9 [1080p x265 10bit WEBRip PSA]/"  --drive-server-side-across-configs ; done
+#for i in $(cat Doom720 | uniq) ; do rclone --config rclone.conf move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/Doom Patrol/Doom Patrol Season 3 (720p x265 10bit WEBRip PSA)/" --drive-server-side-across-configs ; done
+#for i in $(cat Doom1080 | uniq) ; do rclone --config rclone.conf move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/Doom Patrol/Doom Patrol Season 3 (1080p x265 10bit WEBRip PSA)/" --drive-server-side-across-configs ; done
+for i in $(cat Legacies720 | uniq) ; do rclone --config rclone.conf move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/Legacies/Legacies Season 4 (720p x265 10bit WEBRip PSA)/"  --drive-server-side-across-configs ; done
+for i in $(cat Legacies1080 | uniq) ; do rclone --config rclone.conf move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/Legacies/Legacies Season 4 (1080p x265 10bit WEBRip PSA)/"  --drive-server-side-across-configs ; done
+for i in $(cat Wheel720 | uniq) ; do rclone --config rclone.conf move  -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/The Wheel of Time/Season 1 [720p x265 10bit WEBRip PSA]/" --drive-server-side-across-configs ; done
+for i in $(cat Wheel1080 | uniq) ; do rclone --config rclone.conf move  -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/The Wheel of Time/Season 1 [1080p x265 10bit WEBRip PSA]/" --drive-server-side-across-configs ; done
+for i in $(cat Mushoku | uniq) ; do rclone --config rclone.conf move  -P D1:Archive/Unsorted/"$i" D1:"Archive/Anime/Mushoku Tensei/" --drive-server-side-across-configs ; done
+for i in $(cat Kimetsu | uniq) ; do rclone --config rclone.conf move -P D1:Archive/Unsorted/"$i" D1:"Archive/Anime/Demon Slayer (Kimetsu no Yaiba)/" --drive-server-side-across-configs ; done
+for i in $(cat Dexter720 | uniq) ; do rclone --config rclone.conf move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/Dexter/Dexter Season 9 [720p x265 10bit WEBRip PSA]/"  --drive-server-side-across-configs ; done
+for i in $(cat Dexter1080 | uniq) ; do rclone --config rclone.conf move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/Dexter/Dexter Season 9 [1080p x265 10bit WEBRip PSA]/"  --drive-server-side-across-configs ; done
 
-rm Mushoku Kimetsu Doom720 Doom1080 Legacies720 Legacies1080 Wheel720 Wheel1080 Dexter720 Dexter1080
+rm Mushoku Kimetsu Legacies720 Legacies1080 Wheel720 Wheel1080 Dexter720 Dexter1080
 
 ##########################################################################################################################################
 ##########################################################################################################################################
