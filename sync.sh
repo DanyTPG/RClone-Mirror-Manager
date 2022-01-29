3f9cca9fcfc1fd0ea811cc217e8db1d0bbaf7a4f
DanyTPG <50212295+DanyTPG@users.noreply.github.com>
2022-01-29 23:38:47 +0330

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 rclone --config rclone.conf lsf D1:Archive/Unsorted/ > list
 
-#cat list | grep  Mushoku.*\.mkv >> Mushoku
 cat list | grep  Kimetsu.*\.mkv >> Kimetsu
 cat list | grep  Demon.*\.mkv >> Demon
 cat list | grep  Blade.*720.*mkv >> Blade720
@@ -40,7 +39,6 @@ for i in $(cat Legacies720 | uniq) ; do rclone --config rclone.conf move -P D1:A
 for i in $(cat Legacies1080 | uniq) ; do rclone --config rclone.conf move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/Legacies/Legacies Season 4 (1080p x265 10bit WEBRip PSA)/"  --drive-server-side-across-configs ; done
 for i in $(cat AttackPSA | uniq) ; do rclone --config rclone.conf move  -P D1:Archive/Unsorted/"$i" D1:"Archive/Anime/Attack on Titan/Season 4/PSA/" --drive-server-side-across-configs ; done
 for i in $(cat AttackJudas | uniq) ; do rclone --config rclone.conf move  -P D1:Archive/Unsorted/"$i" D1:"Archive/Anime/Attack on Titan/Season 4/Judas/" --drive-server-side-across-configs ; done
-#for i in $(cat Mushoku | uniq) ; do rclone --config rclone.conf move  -P D1:Archive/Unsorted/"$i" D1:"Archive/Anime/Mushoku Tensei/[Judas] Mushoku Tensei - Isekai Ittara Honki Dasu (Season 1) [1080p][HEVC x265 10bit][Multi-Subs]/" --drive-server-side-across-configs ; done
 for i in $(cat Demon | uniq) ; do rclone --config rclone.conf move -P D1:Archive/Unsorted/"$i" D1:"Archive/Anime/Demon Slayer (Kimetsu no Yaiba)/[PSA] Kimetsu no Yaiba (Season 3) [WEBRip 1080p][HEVC x265 10bit]/" --drive-server-side-across-configs ; done
 for i in $(cat Kimetsu | uniq) ; do rclone --config rclone.conf move -P D1:Archive/Unsorted/"$i" D1:"Archive/Anime/Demon Slayer (Kimetsu no Yaiba)/[Judas] Kimetsu no Yaiba (Season 3) [BD 1080p][HEVC x265 10bit][Dual-Audio][Multi-Subs]/" --drive-server-side-across-configs ; done
 for i in $(cat Dexter720 | uniq) ; do rclone --config rclone.conf move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/Dexter New Blood/Season 1 [x265 10bit WEBRip PSA]/720p/"  --drive-server-side-across-configs ; done
@@ -71,7 +69,6 @@ for i in $(cat p | uniq); do rclone --config rclone.conf copy D1:Archive/Unsorte
 for i in $(cat p | uniq); do rclone --config rclone.conf delete D1:Archive/Unsorted/"$i" ; done
 rm p p1440 list
 
-
 rclone --config rclone.conf sync  D1:Archive D2:Archive --drive-server-side-across-configs --checksum
 rclone --config rclone.conf sync  D1:Archive D3:Archive --drive-server-side-across-configs --checksum
 rclone --config rclone.conf sync  D1:Archive D4:Archive --drive-server-side-across-configs --checksum
@@ -81,5 +78,3 @@ rclone --config rclone.conf sync  D1:Archive D5:Archive --drive-server-side-acro
 rclone --config rclone.conf sync  D6:Folder D3:Folder --drive-server-side-across-configs --checksum
 rclone --config rclone.conf sync  D6:Folder D4:Folder --drive-server-side-across-configs --checksum
 rclone --config rclone.conf sync  D6:Folder D5:Folder --drive-server-side-across-configs --checksum
-
-date
