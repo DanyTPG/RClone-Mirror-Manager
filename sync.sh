7a365c9b761713d532e364e9fdd83bc19f7c0929
DanyTPG <50212295+DanyTPG@users.noreply.github.com>
2022-04-04 16:15:58 +0430

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 rclone --config rclone.conf lsf D1:Archive/Unsorted/ -R > list
 #cat list | grep \\[SubsPlease\\].*Shingeki.*\.mkv > subsAttack
-cat list | grep  Peaky.*720p.*\.mkv >> Peaky720
-cat list | grep  Peaky.*1080p.*PSA\.mkv >> Peaky1080x
+
 cat list | grep  Peaky.*1080p.*264.*\.mkv >> Peaky1080
 cat list | grep  Peaky.*2160p.*\.mkv >> Peaky2160
 cat list | grep  Eve.*720p.*\.mkv >> Eve720
@@ -30,8 +29,6 @@ for i in $(cat Superman720 | uniq) ; do rclone --config rclone.conf move -P D1:A
 for i in $(cat Superman1080 | uniq) ; do rclone --config rclone.conf move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/Superman and Lois/Season 2 [1080p x265 10bit WEBRip PSA]/"  --drive-server-side-across-configs ; done
 for i in $(cat Eve720 | uniq) ; do rclone --config rclone.conf move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/Killing Eve/Season 4/720p/"  --drive-server-side-across-configs ; done
 for i in $(cat Eve1080 | uniq) ; do rclone --config rclone.conf move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/Killing Eve/Season 4/1080p/"  --drive-server-side-across-configs ; done
-for i in $(cat Peaky720 | uniq) ; do rclone --config rclone.conf move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/Peaky Blinders/Season 6/720p x265 PSA/"  --drive-server-side-across-configs ; done
-for i in $(cat Peaky1080x | uniq) ; do rclone --config rclone.conf move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/Peaky Blinders/Season 6/1080p x265 10bit PSA/"  --drive-server-side-across-configs ; done
 for i in $(cat Peaky1080 | uniq) ; do rclone --config rclone.conf move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/Peaky Blinders/Season 6/1080p/"  --drive-server-side-across-configs ; done
 for i in $(cat Peaky2160 | uniq) ; do rclone --config rclone.conf move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/Peaky Blinders/Season 6/4K 2160p/"  --drive-server-side-across-configs ; done
 for i in $(cat Halo720 | uniq) ; do rclone --config rclone.conf move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/Halo/720p/"  --drive-server-side-across-configs ; done
@@ -39,7 +36,7 @@ for i in $(cat Halo1080 | uniq) ; do rclone --config rclone.conf move -P D1:Arch
 for i in $(cat Halo2160 | uniq) ; do rclone --config rclone.conf move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/Halo/2160p/"  --drive-server-side-across-configs ; done
 
 rm Legacies720 Legacies1080  Eve720 Eve1080 Halo720 Halo1080 Halo2160
-rm Moon2160 Moon1080 Moon720 Superman720 Superman1080 Peaky720 Peaky1080x Peaky1080 Peaky2160
+rm Moon2160 Moon1080 Moon720 Superman720 Superman1080  Peaky1080 Peaky2160
 
 for i in $(cat list | grep .*\.txt) ; do rclone delete D1:"Archive/Unsorted/$i"; done
 
