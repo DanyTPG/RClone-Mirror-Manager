abfc425154cdf6711c1e7aacd5da91a6b62816eb
DanyTPG <50212295+DanyTPG@users.noreply.github.com>
2022-04-24 07:25:16 +0430

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -5,6 +5,7 @@ rclone --config rclone.conf lsf D1:Archive/Unsorted/ -R > list
 cat list | grep \\[Judas\\].*Spy.*\.mkv > spy
 cat list | grep  Saul.*720p.*\.mkv >> Saul720
 cat list | grep  Saul.*1080p.*\.mkv >> Saul1080
+cat list | grep  Saul.*2160p.*\.mkv >> Saul2160
 cat list | grep  Halo.*720p.*\.mkv >> Halo720
 cat list | grep  Halo.*1080p.*\.mkv >> Halo1080
 cat list | grep  Halo.*2160p.*\.mkv >> Halo2160
@@ -16,7 +17,7 @@ cat list | grep Moon.*2160p.*\.mkv >> Moon2160
 cat list | grep  Superman.*720p.*\.mkv >> Superman720
 cat list | grep  Superman.*1080p.*\.mkv >> Superman1080
 
-UPDATED=$(cat spy Saul720 Saul1080 Halo720 Halo1080 Halo2160 Legacies1080 Legacies720 Moon720 Moon1080 Moon2160 Superman720 Superman1080 | grep . -c)
+UPDATED=$(cat spy Saul720 Saul1080 Saul2160 Halo720 Halo1080 Halo2160 Legacies1080 Legacies720 Moon720 Moon1080 Moon2160 Superman720 Superman1080 | grep . -c)
 
 if [ $UPDATED != 0 ]; then
 
@@ -33,12 +34,13 @@ for i in $(cat Halo1080 | uniq) ; do rclone --config rclone.conf move -P D1:Arch
 for i in $(cat Halo2160 | uniq) ; do rclone --config rclone.conf move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/Halo/Season 1/2160p/"  --drive-server-side-across-configs ; done
 for i in $(cat Saul720 | uniq) ; do rclone --config rclone.conf move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/Better Call Saul/Season 6/720p [PSA]/"  --drive-server-side-across-configs ; done
 for i in $(cat Saul1080 | uniq) ; do rclone --config rclone.conf move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/Better Call Saul/Season 6/1080p [PSA]/"  --drive-server-side-across-configs ; done
+for i in $(cat Saul2160 | uniq) ; do rclone --config rclone.conf move -P D1:Archive/Unsorted/"$i" D1:"Archive/Series/Better Call Saul/Season 6/2160p [PSA]/"  --drive-server-side-across-configs ; done
 for i in $(cat spy | uniq) ; do rclone --config rclone.conf move -P D1:Archive/Unsorted/"$i" D1:"Archive/Anime/Spy x Family/Season 1/"  --drive-server-side-across-configs ; done
 
 
 
 
-rm Legacies720 Legacies1080 Halo720 Halo1080 Halo2160 Saul720 Saul1080 Moon2160 Moon1080 Moon720 Superman720 Superman1080 spy
+rm Legacies720 Legacies1080 Halo720 Halo1080 Halo2160 Saul720 Saul1080 Saul2160 Moon2160 Moon1080 Moon720 Superman720 Superman1080 spy
 
 for i in $(cat list | grep .*\.txt) ; do rclone delete D1:"Archive/Unsorted/$i"; done
 
