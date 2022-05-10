975662c4d322f1a8ff193c5fe7a88d536e5da3a8
DanyTPG <50212295+DanyTPG@users.noreply.github.com>
2022-05-10 14:41:53 +0430

    Update pcloud.sh

--- pcloud.sh
+++ pcloud.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rclone --config rclone.conf sync -P  D6:"Folder/PH" pcloud:"Public Folder/PH" --drive-server-side-across-configs --checksum
+rclone --config rclone.conf sync  D6:"Folder/PH" pcloud:"Public Folder/PH" --drive-server-side-across-configs --checksum
